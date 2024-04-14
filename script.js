cc4d7f8d9520bfacf60f849c4bfd1c54775eaf8f
Justin <98992595+JLCurtsinger@users.noreply.github.com>
2024-04-13 20:05:53 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// for animation
+document.addEventListener("DOMContentLoaded", function() {
+    var animationElement = document.getElementById('animation');
+    setTimeout(function() {
+        animationElement.style.opacity = 1;
+    }, 600);
+});
