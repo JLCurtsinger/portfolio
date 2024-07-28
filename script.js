aa7c3e0f913ced593fbf6748877775a76252f110
JLCurtsinger <98992595+JLCurtsinger@users.noreply.github.com>
2024-07-28 09:26:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ document.addEventListener("DOMContentLoaded", function() {
         animationElement.style.opacity = 1;
     }, 600);
 });
+
+// for contact images
+const smallImages = [
+    'images/bellRockSmall.jpg',
+    'images/redwoodsSmall.jpeg'
+];
+
+const mediumImages = [
+    'images/bellRockWide.jpg',
+    'images/redwoodsLarge.jpeg'
+];
+
+const largeImages = [
+    'images/bellRockWide.jpg',
+    'images/redwoodsLarge.jpeg'
+];
